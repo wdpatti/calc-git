2be8bd28669f1dc54a5afc3333fa566f064f60e1
William Patti <wdppostcard@gmail.com>
2025-10-23 14:50:15 -0400

    add basic repeating

--- main.py
+++ main.py
@@ -3,3 +3,13 @@
 num1 = input("Enter the first number: ")
 num2 = input("Enter the second number: ")
 
+while True:
+    #If these numbers are very large or very small nothing should change due to the nature of python's floating point variables
+    #Check validity of num1 and num2 with try: except:
+    try:
+        num1 = float(num1)
+        num2 = float(num2)
+        break
+    except:
+        print("Both num1 and num2 must be able to be expressed as floating point values. Please try again.")
+        continue
